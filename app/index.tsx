18bf3beb7d35caf955533aceb5dec344a7da63b5
r-yvan <yvankiliye.rubuto@gmail.com>
2025-01-20 09:46:40 +0200

    Commit

--- app/index.tsx
+++ app/index.tsx
@@ -2,16 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{
-        color: "white",
-      }}>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
