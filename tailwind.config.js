89eef33f9c976143fe72546063cd1b3642a99ebb
r-yvan <yvankiliye.rubuto@gmail.com>
2025-01-20 10:13:48 +0200

    Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pregular: "",
+        pbold: "",
+        pthin: "",
+        pmedium: "",
+        psemibold: "",
+        pextrabold: "",
+        plight: "",
+        gregular: "",
+        gbold: "",
+        gthin: "",
+        gmedium: "",
+        gsemibold: "",
+        gextrabold: "",
+        glight: "",
+      }
+    },
   },
   plugins: [],
 }
